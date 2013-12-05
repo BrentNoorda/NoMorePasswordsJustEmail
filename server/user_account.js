cf0a26b50920fa0aa16785757cf13055fcda6b71
Brent Noorda <brent.noorda@gmail.com>
2013-12-04 22:50:49 -0800

    add the requested option to allow login via a link, and not just via a security code

--- server/user_account.js
+++ server/user_account.js
@@ -1,12 +1,39 @@
 // javascript server code for managing user registration/login/logout
 
 var loginCodes = new Meteor.Collection("logincodes");
+var Future = Npm.require('fibers/future');
+
+function remove_expired_login_codes() {
+    loginCodes.remove({ timeout: { $lt: (new Date()).valueOf() } });
+}
+
+function login_only_gets_one_chance(email) {
+    loginCodes.remove({email:email});
+}
+
+function login_or_register_user_with_new_password(callbackObj,email) {
+    var user = Meteor.users.findOne({'emails.address':email})
+    var password = Random.hexString(30);
+    var userId;
+    if ( !user ) {
+        userId = Accounts.createUser({email:email,password:password});
+        console.log("CREATED userId = " + userId);
+    } else {
+        userId = user._id;
+        console.log("FOUND userId = " + userId);
+        Accounts.setPassword(userId,password);
+    }
+
+    callbackObj.setUserId(userId);
+
+    return password;
+}
 
 Meteor.methods({
 
     enter_email: function (email) {
         check(email,String);
-        console.log("enter_email for email address: " + email)
+        console.log("enter_email for email address: " + email);
         email = email.toLowerCase();
 
         // determine if this email address is already a user in the system
@@ -31,21 +58,37 @@ Meteor.methods({
         }
         console.log(email + " random code = " + random_code);
 
+        // for those who prefer to login with a link, also create a random string SECURITY_CODE_HASH_LENGTH
+        // characters long
+        var random_hash = "";
+        for ( ; random_hash.length < SECURITY_CODE_HASH_LENGTH; ) {
+            var chr = Random.choice("23456789ABCDEFGHJKLMNPQESTUVWXYZ");
+            if ( random_hash.length !== 0 ) {
+                if ( chr === random_hash.charAt(random_hash.length-1) ) {
+                    continue;
+                }
+            }
+            random_hash += chr;
+        }
+
         // add new record to timeout in LOGIN_CODE_TIMEOUT_MINUTES
         var timeout = (new Date()).valueOf() + (LOGIN_CODE_TIMEOUT_MINUTES * 60 * 1000);
-        loginCodes.upsert({email:email},{email:email,code:random_code,timeout:timeout});
+        loginCodes.upsert({email:email},{email:email,code:random_code,hash:random_hash,timeout:timeout});
         var codeType = user ? "login" : "registration";
 
         Email.send({
             from: "brent.noorda@gmail.com",
             to: email,
-            subject: "NoMorePasswordsJustEmail " + codeType + " code",
+            subject: "NoMorePasswordsJustEmail " + codeType + " security code",
             text: ( "Your NoMorePasswordsJustEmail " + codeType + " code is:\r\n\r\n      " + random_code + "\r\n\r\n" +
-                    "note: this code is only valid for " + LOGIN_CODE_TIMEOUT_MINUTES + " minutes." ),
+                    "or use this link:\r\n\r\n      " + WEB_URL + "#" + random_hash + "\r\n\r\n" +
+                    "note: this single-use code is only valid for " + LOGIN_CODE_TIMEOUT_MINUTES + " minutes." ),
             html: ( "<html><body>" +
                     '<p>Your <b><i>NoMorePasswordsJustEmail</i></b> ' + codeType + ' code is:</p>' +
-                    '<p><font size="+1"><b>' + random_code + '</b></font></p>' +
-                    '<p><font size="-1">note: this code is only valid for ' + LOGIN_CODE_TIMEOUT_MINUTES + ' minutes.</font></p>' +
+                    '<p style="margin-left:2em;"><font size="+1"><b>' + random_code + '</b></font></p>' +
+                    '<p>or click on this link</p>' +
+                    '<p style="margin-left:2em;"><font size="-1"><a href="' + WEB_URL + '#' + random_hash + '">' + WEB_URL + '#' + random_hash + '</a></font></p>' +
+                    '<p><font size="-1">note: this single-use code is only valid for ' + LOGIN_CODE_TIMEOUT_MINUTES + ' minutes.</font></p>' +
                     '</body></html>' )
         });
 
@@ -56,47 +99,56 @@ Meteor.methods({
     enter_security_code: function (email,code) {
         check(email,String);
         check(code,String);
-        console.log("enter_security_code for email address: " + email + " - code: " + code)
+        console.log("enter_security_code for email address: " + email + " - code: " + code);
         email = email.toLowerCase();
 
         // delete any login codes that have timed out yet
-        loginCodes.remove({ timeout: { $lt: (new Date()).valueOf() } });
+        remove_expired_login_codes();
 
         // If can find this record in login codes then all is well, else it failed
         var loginCode = loginCodes.findOne({email:email,code:code});
+        login_only_gets_one_chance(email);
         if ( !loginCode ) {
-            loginCodes.remove({email:email}); // user only gets one chance
             throw "failed to log in";
         }
-        loginCodes.remove({email:email}); // user only gets one chance
 
-        // if use does not already exist, then create this user
-        var user = Meteor.users.findOne({'emails.address':email})
-        var password = Random.hexString(30);
-        var userId;
-        if ( !user ) {
-            userId = Accounts.createUser({email:email,password:password});
-            console.log("CREATED userId = " + userId);
-        } else {
-            userId = user._id;
-            console.log("FOUND userId = " + userId);
-            Accounts.setPassword(userId,password);
-        }
-
-        this.setUserId(userId);
+        var password = login_or_register_user_with_new_password(this,email);
 
         return password;
     },
 
     cancel_login_code: function (email) {
         check(email,String);
-        console.log("cancel_login_code for email address: " + email)
+        console.log("cancel_login_code for email address: " + email);
         email = email.toLowerCase();
 
         // delete any existing record for this user login codes
-        loginCodes.remove({email:email});
+        login_only_gets_one_chance(email);
 
         return "ok";
+    },
+
+    login_via_url: function (hash) {
+        check(hash,String);
+        console.log("login_via_url for hash: " + hash);
+
+        var loginCode = loginCodes.findOne({hash:hash});
+        if ( loginCode ) {
+            login_only_gets_one_chance(loginCode.email);
+        }
+
+        // I don't want bots just brute-force attacking the server to guess the login access. To prevent
+        // that I'll force a minor little delay, minor enough to not bother a real user but enough to
+        // relay annoy a billion bots. The make-it-look-synchronous part of this delate is taken from
+        // what I learned at: https://gist.github.com/possibilities/3443021
+        var future = new Future();
+        setTimeout(function() { future.return(); }, 333);
+        future.wait();
+
+        if ( !loginCode ) {
+            throw "blech; invalid code"
+        }
+        return {email:loginCode.email,pwd:login_or_register_user_with_new_password(this,loginCode.email)};
     }
 
 });
